84919c895f99a1fe4e34d833c09121ce60980a4c
Amala George <amala.george.ed@gmail.com>
2025-01-06 15:40:49 +0530

    increased explicit sleep time

--- src/test/java/com/assignment/steps/StepDefinitions.java
+++ src/test/java/com/assignment/steps/StepDefinitions.java
@@ -89,7 +89,7 @@ public class StepDefinitions extends TestBase{
 
     @Then("Cover images of articles are downloaded")
     public void cover_images_of_articles_are_downloaded() throws IOException, InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(55));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(85));
 
         for(int i = 0; i < 5; i++){
 
